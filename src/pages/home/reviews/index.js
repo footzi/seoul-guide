d829ace4299b4da35fb1db8d83217bae04a3cac7
Vladislav Manakov <vladislav.manakov@fundraiseup.com>
2024-12-16 08:53:09 +0100

    Changes

--- src/pages/home/reviews/index.js
+++ src/pages/home/reviews/index.js
@@ -11,7 +11,7 @@ export class ReviewsSlider {
   init() {
     new Swiper('.js-reviews', {
       slidesPerView: 1,
-      spaceBetween: 12,
+      spaceBetween: 36,
       grabCursor: true,
       modules: [Navigation],
       breakpoints: {
@@ -19,8 +19,13 @@ export class ReviewsSlider {
           slidesPerView: 1.5,
         },
         1200: {
-          slidesPerView: 2.5,
+          slidesPerView: 2.3,
+          spaceBetween: 28,
+          initialSlide: 2,
         },
+        1600: {
+          slidesPerView: 2.7,
+        }
       },
       navigation: {
         nextEl: '.js-reviews__button-next',

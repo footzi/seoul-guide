ba1c36bf6b2b46b3567805dd9a26dfe6d72e186e
Vladislav Manakov <vladislav.manakov@fundraiseup.com>
2024-12-17 18:17:34 +0100

    Changes

--- deploy/prod.bash
+++ deploy/prod.bash
@@ -5,6 +5,6 @@ ssh -p $DEPLOY_PORT $DEPLOY_URL "
   cd $DEPLOY_FOLDER &&
   git reset --hard origin/master &&
   git pull &&
-  sudo -S yarn &&
-  sudo -S yarn build
+  sudo -S npm ci &&
+  sudo -S npm run build
 "

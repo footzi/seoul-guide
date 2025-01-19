b7e00023db78432527db50e563fcd7dfe0be0e39
Vladislav Manakov <vladislav.manakov@fundraiseup.com>
2025-01-19 11:33:45 +0100

    Fix template

--- deploy/prod.bash
+++ deploy/prod.bash
@@ -5,6 +5,6 @@ ssh -p $DEPLOY_PORT $DEPLOY_URL "
   cd $DEPLOY_FOLDER &&
   git reset --hard origin/master &&
   git pull &&
-  npm ci &&
-  npm run build
+  sudo -S npm ci &&
+  sudo -S npm run build
 "
